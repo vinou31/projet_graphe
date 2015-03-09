92571212a82dafd410da9ef6c9b1db138d17282c
Boucheraa Alvin <aboucher2@knuth.enseeiht.fr>
2015-03-09 17:16:16 +0100

    modification de clarté + ébauche d'utilisation d'un fold

--- projet.ml
+++ projet.ml
@@ -19,6 +19,7 @@ open DAG;;
 
 
 let rec print_vertex lv = List.fold_right (fun v x -> (V.label v)::x) lv [];;
+let rec print_mark_v lv = List.fold_right (fun v x -> (Mark.get v)::x) lv [];;
 
 let rec is_include l1 l2 = 
 	match l1 with
@@ -42,32 +43,52 @@ let v_sansDep dag =
 
 
 
-let rec print_mark_v lv = List.fold_right (fun v x -> (Mark.get v)::x) lv [];;
 
+
+(*
 let tri_topologique dag =
 	let y = v_sansDep dag in
-				let rec aux vi i z = 			
+				let rec constructeur_z vi i z = 			
 					match vi with
 					|[]-> z(*retourner z*)
-					|h::t-> 				
-						begin (*numéroter h, ajouter h à z*)	
-							(*Mark.set h i;*)							
+					|h::t->	begin 
+							(*numéroter h, ajouter h à z*)	
+							Mark.set h i;						
 							(*manque une fonction recursive pour parcourir tout vj*)
 							let vj = succ dag h in
-								let rec aux2 lvj lvi lz=  
+								let rec verification lvj lvi lz=  
 								match lvj with
-								|[]-> aux (t@lvi) (i+1) (lz)
+								|[]-> constructeur_z (t@lvi) (i+1) (lz)
 								|a::b-> if is_include (pred dag a) lz then
 												(*aux a::t*)
-												aux2 b (lvi@[a]) lz											
+												verification b (lvi@[a]) lz											
 											else
-												aux2 b lvi lz
-							in aux2 vj [] (h::z);
+												verification b lvi lz
+							in verification vj [] (h::z);
 						end						
-				in aux y 1 []
+				in constructeur_z y 1 []
 ;;				
-
-
+*)
+(* Essaie de codage avec un fold*)
+let tri_topologique dag =
+	let y = v_sansDep dag in
+				let rec constructeur_z vi i z = 			
+					match vi with
+					|[]-> z(*retourner z*)
+					|h::t->	begin 
+							(*numéroter h, ajouter h à z*)	
+							Mark.set h i;
+							let new_z = h::z in	
+								let successeur = succ dag h in
+									let new_vi = List.fold_right (fun v x ->if is_include (pred dag v) new_z then
+																		x::v										
+																	else
+																		x)
+																  successeur vi in
+									constructeur_z new_vi (i+1) new_z;
+							end
+				in constructeur_z y 1 []
+;;	
 
 
 (* trace d'execution 
@@ -109,48 +130,22 @@ let rec find_next_step dag  etapePrecedente nextStep =
 				in (analyse lSucc)::(find_next_step dag la nextStep)
 ;;
 
-let rec build_trace dag trace = 
-	let trace = [v_sansDep dag] in
-		match trace with
-			|[] -> build_trace dag ([v_sansDep dag]::trace)
+let rec ordonnanceur_ressources_illimitees dag  = 
+	let build_trace = [v_sansDep dag] in
+		match build_trace with
+			|[] -> trace dag ([v_sansDep dag]::build_trace)
 			|[a]-> let next = find_next_step dag a [] in
 				if ((next)=[]) then
 					trace
 				else
-					build_trace dag ((next)::trace)
+					trace dag ((next)::trace)
 			|etape::autres_etapes -> let next = find_next_step dag a [] in
 				if ((next)=[]) then
-					trace
+					build_trace
 				else
-					build_trace dag ((next)::trace)
+					trace dag ((next)::build_trace)
 ;;				
 *)
-
-(*
-let ordonnanceur_ressources_illimitees dag =
-	let y = fold_vertex (fun v x -> v::x) dag [] in
-		let trace = [] in
-			let i = 1 in
-				let rec aux vi = 
-					let etage = [] in		
-					match vi with
-					|[]-> (*retourner z*)
-					|h::t-> 
-						begin (*numéroter h, ajouter h à z*)	
-							h::etage;
-							let vj = succ h in
-								match vj with
-								|[]-> aux t
-								|a::b-> if List.mem (prec a) z then
-												(*aux a::t*)
-												aux t@[a]
-											else
-												aux t
-						end
-				in aux y
-;;				
-*)
-
 
 (* entrees: 
    - un nombre entier de ressources
